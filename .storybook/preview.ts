e6f7713d06ee7e8df5dfb29537673a6033f4cdb4
Fernanda Aguilar <fernanda@trazolabs.com>
2025-04-13 21:43:05 -0400

    add: custom viewports

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,33 @@
-import type { Preview } from '@storybook/react'
+import type { Preview } from "@storybook/react";
+
+const customViewports = {
+  smallDevice: {
+    name: "smallDevice",
+    styles: {
+      width: "360px",
+      height: "540px",
+    },
+  },
+  mediumDevice: {
+    name: "mediumDevice",
+    styles: {
+      width: "1024px",
+      height: "1200px",
+    },
+  },
+};
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
+    },
+    viewport: {
+      viewports: customViewports,
+      defaultViwport: "someDefault",
     },
   },
 };
